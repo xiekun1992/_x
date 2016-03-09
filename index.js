5a8682d1a164c0ec1cae1e6751acbb5f86f3d9de
xiekun1992 <840914927@qq.com>
2016-03-09 23:42:26 +0800

    add test for subscribe,showEvents and publish methods, and the implementation

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 'use strict';
 
 function Observer(){
+	var events={};
 
-}
-Observer.prototype.publish=function(){
+	this.publish=function(eventName){
+		var cbArray=events[eventName];
+		for(var i=0;i<cbArray.length;i++){
+			cbArray[i]();
+		}
+	}
+	this.unsubscribe=function(){
 
-}
-Observer.prototype.unsubscribe=function(){
+	}
+	this.subscribe=function(eventName,callback){
+		if(!events[eventName]){
+			events[eventName]=[];
+		}
+		events[eventName].push(callback);
+	}
+	this.showEvents=function(){
+		var ets=[];
+		for(var e in events){
+			ets.push(e);
+		}
+		return ets;
+	}
 
-}
-Observer.prototype.subscribe=function(){
-
+	return {
+		publish:this.publish,
+		unsubscribe:this.unsubscribe,
+		subscribe:this.subscribe,
+		showEvents:this.showEvents
+	}
 }
--- test/specs.js
+++ test/specs.js
@@ -7,9 +7,37 @@ describe('when initialized',function(){
 		observer=new Observer();
 	});
 
-	it('Observer should have publish,unsubscribe and subscribe methods',function(){
+	it('Observer should have showEvents,publish,unsubscribe and subscribe methods',function(){
 		expect(observer.publish).toBeDefined();
 		expect(observer.unsubscribe).toBeDefined();
 		expect(observer.subscribe).toBeDefined();
+		expect(observer.showEvents).toBeDefined();
+	});
+});
+
+describe('when subscribe event LOGIN,',function(){
+	var cb;
+	var observer;
+
+	beforeAll(function(){
+		cb=jasmine.createSpy("cb");
+		observer=new Observer();
+	});
+	afterAll(function(){
+		observer=null;
+	});
+	it('subscribe method should be called with string LOGIN and a callback function',function(){
+		spyOn(observer,'subscribe').and.callThrough();
+		observer.subscribe('LOGIN',cb);
+		expect(observer.subscribe).toHaveBeenCalledWith('LOGIN',cb);
+	});
+	it('call showEvents method will return all the events subscribed',function(){
+		expect(observer.showEvents()).toEqual(['LOGIN']);
+	});
+	describe('when publish an event LOGIN,',function(){
+		it('the callback function related to LOGIN should be executed',function(){
+			observer.publish('LOGIN');
+			expect(cb).toHaveBeenCalled();
+		});
 	})
-})
+});
