37f6581adcdcf92675f6928f3a65531b5463a7e7
xiekun1992 <840914927@qq.com>
2016-03-15 23:54:16 +0800

    add gulp

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      './index.js',
+      './src/_x.js',
       './test/specs.js'
     ],
 
