7d639ff1a454888b4c05e4635cda376728eaaa36
xiekun1992 <xkfront@outlook.com>
2018-08-25 00:54:00 +0800

    add base16 and base64 encoding test specs and finish this function

---
+++ src/base_encode.js
@@ -0,0 +1,88 @@
+// https://tools.ietf.org/html/rfc4648
+'use strict';
+function BaseEncode() {
+  var base64Alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+  var base64SafeAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_';
+  var base32Alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567';
+  var base32hexAlphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUV';
+  var base16Alphabet = '0123456789ABCDEF';
+
+  function inputToBinary(input) {
+    var str = input.toString();
+    var binary = '';
+    for (var i = 0; i < str.length; i++) {
+      var code = str.charCodeAt(i);
+      binary += padStart(code.toString(2), 8, '0');
+    }
+    return binary;
+  }
+  function padStart(str, targetLength, padString) {
+    if (str.length < targetLength) {
+      for (var i = str.length; i < targetLength; i++) {
+        str = padString + str;
+      }
+    }
+    return str;
+  }
+  function padEnd(str, targetLength, padString) {
+    if (str.length < targetLength) {
+      for (var i = str.length; i < targetLength; i++) {
+        str += padString;
+      }
+    }
+    return str;
+  }
+  function divideBinary(binaryStr, bitPerGroup, padAtEnd) {
+    var arr = [];
+    var rounds = Math.ceil(binaryStr.length / bitPerGroup);
+    for (var i = 0, j = bitPerGroup; 0 < rounds--; i += bitPerGroup) {
+      var substr = binaryStr.slice(i, i + j);
+      // console.log(substr, i, i + j);
+      if (padAtEnd) {
+        substr = padEnd(substr, bitPerGroup, '0')
+      }
+      arr.push(substr);
+    }
+    return arr;
+  }
+  return {
+    base64: function(input) {
+      var binaryStr = inputToBinary(input);
+      var binaryGroupBefore = divideBinary(binaryStr, 3 * 8);
+      var binaryGroupAfter = [];
+      for (var i = 0; i < binaryGroupBefore.length; i++) {
+        let g = divideBinary(binaryGroupBefore[i], 6, true);
+        binaryGroupAfter.push(g);
+      }
+      var res = '';
+      for (var i = 0; i < binaryGroupAfter.length; i++) {
+        if (binaryGroupAfter[i]) {
+          for (var j = 0; j < 4; j++) {
+            var binaryS = binaryGroupAfter[i][j];
+            if (binaryS) {
+              res += base64Alphabet[parseInt(binaryS, 2)];
+            } else {
+              res += '=';
+            }
+          }
+        }
+      }
+      return res;  
+    },
+    base32: function(input) {
+    
+    },
+    base32hex: function(input) {
+    
+    },
+    base16: function(input) {
+      var binaryStr = inputToBinary(input);
+      var binaryGroup = divideBinary(binaryStr, 4);
+      var res = '';
+      for (var i = 0; i < binaryGroup.length; i++) {
+        res += base16Alphabet[parseInt(binaryGroup[i], 2)];
+      }
+      return res;
+    }
+  }
+}
---
+++ test/base_encode_specs.js
@@ -0,0 +1,98 @@
+'use strict';
+
+var baseEncode = new BaseEncode();
+describe('when use base64,', function() {
+  it('"" should equal ""', function() {
+    expect(baseEncode.base64("")).toEqual("");
+  });
+  it('f should equal Zg==', function() {
+    expect(baseEncode.base64("f")).toEqual("Zg==");
+  });
+  it('fo should equal Zm8=', function() {
+    expect(baseEncode.base64("fo")).toEqual("Zm8=");
+  });
+  it('foo should equal Zm9v', function() {
+    expect(baseEncode.base64("foo")).toEqual("Zm9v");
+  });
+  it('foob should equal Zm9vYg==', function() {
+    expect(baseEncode.base64("foob")).toEqual("Zm9vYg==");
+  });
+  it('fooba should equal Zm9vYmE=', function() {
+    expect(baseEncode.base64("fooba")).toEqual("Zm9vYmE=");
+  });
+  it('foobar should equal Zm9vYmFy', function() {
+    expect(baseEncode.base64("foobar")).toEqual("Zm9vYmFy");
+  });
+});
+
+// describe('when use base32,', function() {
+//   it('"" should equal ""', function() {
+//     expect(baseEncode.base32("")).toEqual("");
+//   });
+//   it('f should equal MY======', function() {
+//     expect(baseEncode.base32("f")).toEqual("MY======");
+//   });
+//   it('fo should equal MZXQ====', function() {
+//     expect(baseEncode.base32("fo")).toEqual("MZXQ====");
+//   });
+//   it('foo should equal MZXW6===', function() {
+//     expect(baseEncode.base32("foo")).toEqual("MZXW6===");
+//   });
+//   it('foob should equal MZXW6YQ=', function() {
+//     expect(baseEncode.base32("foob")).toEqual("MZXW6YQ=");
+//   });
+//   it('fooba should equal MZXW6YTB', function() {
+//     expect(baseEncode.base32("fooba")).toEqual("MZXW6YTB");
+//   });
+//   it('foobar should equal MZXW6YTBOI======', function() {
+//     expect(baseEncode.base32("foobar")).toEqual("MZXW6YTBOI======");
+//   });
+// });
+
+// describe('when use base32hex,', function() {
+//   it('"" should equal ""', function() {
+//     expect(baseEncode.base32hex("")).toEqual("");
+//   });
+//   it('f should equal CO======', function() {
+//     expect(baseEncode.base32hex("f")).toEqual("CO======");
+//   });
+//   it('fo should equal CPNG====', function() {
+//     expect(baseEncode.base32hex("fo")).toEqual("CPNG====");
+//   });
+//   it('foo should equal CPNMU===', function() {
+//     expect(baseEncode.base32hex("foo")).toEqual("CPNMU===");
+//   });
+//   it('foob should equal CPNMUOG=', function() {
+//     expect(baseEncode.base32hex("foob")).toEqual("CPNMUOG=");
+//   });
+//   it('fooba should equal CPNMUOJ1', function() {
+//     expect(baseEncode.base32hex("fooba")).toEqual("CPNMUOJ1");
+//   });
+//   it('foobar should equal CPNMUOJ1E8======', function() {
+//     expect(baseEncode.base32hex("foobar")).toEqual("CPNMUOJ1E8======");
+//   });
+// });
+
+describe('when use base16,', function() {
+  it('"" should equal ""', function() {
+    expect(baseEncode.base16("")).toEqual("");
+  });
+  it('f should equal 66', function() {
+    expect(baseEncode.base16("f")).toEqual("66");
+  });
+  it('fo should equal 666F', function() {
+    expect(baseEncode.base16("fo")).toEqual("666F");
+  });
+  it('foo should equal 666F6F', function() {
+    expect(baseEncode.base16("foo")).toEqual("666F6F");
+  });
+  it('foob should equal 666F6F62', function() {
+    expect(baseEncode.base16("foob")).toEqual("666F6F62");
+  });
+  it('fooba should equal 666F6F6261', function() {
+    expect(baseEncode.base16("fooba")).toEqual("666F6F6261");
+  });
+  it('foobar should equal 666F6F626172', function() {
+    expect(baseEncode.base16("foobar")).toEqual("666F6F626172");
+  });
+});
